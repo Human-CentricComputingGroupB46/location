d015062b5434e0d9c669b932253421322f0bc74e
apple <nbj@fcg>
2026-03-16 02:21:30 +0800

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by psg on 2026/3/16.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocationTests/LocationTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocationTests.swift
+//  LocationTests
+//
+//  Created by psg on 2026/3/16.
+//
+
+import Testing
+@testable import Location
+
+struct LocationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
